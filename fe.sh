163bba438af29397ad44ed6f46482566296ec8aa
annama2019 <55451198+annama2019@users.noreply.github.com>
2021-06-14 21:59:40 -0400

    Create fe.sh

---
+++ fe.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -J fe
+#SBATCH -o fe_out.out
+#SBATCH -e fe_out.err
+#SBATCH -p shared
+#SBATCH -N 1
+#SBATCH -c 1
+#SBATCH -t 0-00:30
+#SBATCH --mem=2G
+
+# Load required software modules
+module load intel/17.0.4-fasrc01
+module load julia/1.3.1-fasrc01
+srun -n 1 -c 1 julia fe.jl
