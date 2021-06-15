56f4a71aa82fb219e6b034c232be2f0f95715983
annama2019 <55451198+annama2019@users.noreply.github.com>
2021-06-15 16:08:09 -0400

    Update plot.jl

--- plot.jl
+++ plot.jl
@@ -2,3 +2,8 @@ using Plots
 
 # plot some data
 plot([cumsum(rand(500) .- 0.5), cumsum(rand(500) .- 0.5)])
+
+# save the current figure
+savefig("plots.svg")
+# .eps, .pdf, & .png are also supported
+# we used svg here because it respects the width and height specified above
