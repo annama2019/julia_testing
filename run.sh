aff25b262da0dd199e115dd8c0a3fafbe04026e5
annama2019 <55451198+annama2019@users.noreply.github.com>
2021-06-14 22:00:37 -0400

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -J pi_monte_carlo
+#SBATCH -o pi_monte_carlo.out
+#SBATCH -e pi_monte_carlo.err
+#SBATCH -p shared
+#SBATCH -N 1
+#SBATCH -c 1
+#SBATCH -t 0-00:30
+#SBATCH --mem=2G
+
+# Load required software modules
+module load julia/1.1.1-fasrc01
+srun -n 1 -c 1 julia julia_test.jl
